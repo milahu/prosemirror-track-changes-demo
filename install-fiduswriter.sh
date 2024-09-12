8010bcb1f6ad43ac14b760ed2d9cfcc9abab37a2
Milan Hauth <milahu@gmail.com>
2024-09-12 15:51:04 +0200

    up install-fiduswriter.sh

--- install-fiduswriter.sh
+++ install-fiduswriter.sh
@@ -10,9 +10,7 @@ set -e # fail on errors
 which mmv >/dev/null || { echo please install mmv; exit 1; }
 which git >/dev/null || { echo please install git; exit 1; }
 which sed >/dev/null || { echo please install sed; exit 1; }
-which python3 >/dev/null || { echo please install python3; exit 1; }
-which pip3 >/dev/null || { echo please install pip3; exit 1; }
-#which ncu >/dev/null || { echo please install ncu: npm i -g npm-check-updates; exit 1; }
+which ncu >/dev/null || { echo please install ncu: npm i -g npm-check-updates; exit 1; }
 
 fwdir=github.com/fiduswriter/fiduswriter
 
@@ -36,46 +34,25 @@ fwdir=github.com/fiduswriter/fiduswriter
 mkdir -p $(dirname $fwdir) || true
 (
   cd $(dirname $fwdir)
+  ! [ -e fiduswriter ] &&
   git clone --depth 1 --branch $fiduswriter_branch https://github.com/fiduswriter/fiduswriter.git
   cd fiduswriter
 
   cd fiduswriter
-
-  # REVOKE this is too aggressive -> update-prosemirror-model-to-1.14.1.diff
-  false && {
-    # update deps
-    find . -name package.json \
-    | while read package_json
-    do
-      package_dir=$(dirname "$package_json")
-      (
-        cd "$package_dir"
-        echo "run 'ncu -u' in $(pwd)"
-        # 'echo |' otherwise ncu complains about invalid stdin
-        echo | ncu --upgrade
-      )
-    done
-    #done < <(find . -name package.json)
-  }
 
   # https://github.com/fiduswriter/fiduswriter/wiki/Installation-for-developers
 
   cp configuration-default.py configuration.py
 
   # debian
-  which apt >/dev/null && {
+  which apt &>/dev/null && {
     sudo apt install libjpeg-dev python3-dev python3-pip gettext zlib1g-dev git npm nodejs build-essential
     python3 manage.py setup
   }
 
   # nixos
-  which nix-shell >/dev/null && {
-    # some python modules are missing in nixpkgs
-    cat requirements.txt | grep -v -i -E '(bleach|django|django-allauth|pillow|python-dateutil|python-magic|tornado|jsonpatch)==' >requirements.filtered.txt
-    pip3 install -r requirements.filtered.txt
-
-    nix-shell -p gettext python38Packages.{bleach,django,django-allauth} \
-      python38Packages.{pillow,python-dateutil,python_magic,tornado,jsonpatch} \
+  which nix-shell &>/dev/null && {
+    nix-shell ../../../../shell.nix \
       --run 'python3 manage.py setup'
   }
 
@@ -87,43 +64,45 @@ mkdir -p $(dirname $fwdir) || true
   # in fiduswriter/fiduswriter
   # "fix" import paths
   # symlink all files into the document package
-  echo -e '\nmerge packages (apply migrations) ...'
+  echo -e '\nmerging packages (apply migrations) ...'
   find . -mindepth 2 -name '*.js' | cut -d/ -f2 | uniq \
   | grep -v -e document -e '\.transpile' -e static-libs | while read package
   do
-    echo -e "\nmerge $package/ to document/"
+    echo -e "\nmerging $package/ to document/"
     # cp -s needs absolute source path
     cp -asv "$(readlink -f $package)"/*/ ./document/ || true
     # allow to fail: "symbolic link exists"
   done
 
   package=static-libs
-  echo -e "\nmerge $package/ to document/static/"
+  echo -e "\nmerging $package/ to document/static/"
   cp -asv "$(readlink -f $package)"/*/ ./document/static/ || true
 
   # symlink init.js to index.js
   find . -name init.js | while read f; do
     d=$(dirname "$f");
-    if [ -f "$d/index.js" ]; then echo "skip $f"; continue; fi;
-    echo "$d";
+    if [ -f "$d/index.js" ]; then echo "skipping $f"; continue; fi;
+    echo "adding $d";
     ln -sv init.js $d/index.js;
   done
 
   # Uncaught ReferenceError: gettext is not defined
   # see fiduswriter/fiduswriter/document/management/commands/export_schema.py
   # prepend "var gettext = ..." before first line
-  echo -e '\nadd mock for gettext'
+  echo -e '\nadding mock for gettext'
   sed -i.bak '1s;^;var gettext = () => undefined\n\n;' document/static/js/modules/*/*.js
   sed -i.bak '1s;^;var gettext = () => undefined\n\n;' document/static/js/modules/*/*/*.js
   sed -i.bak '1s;^;var gettext = () => undefined\n\n;' document/static/js/modules/*/*/*/*.js
 )
+
+if [ -n "$(find node_modules/citeproc-plus -name '*.csljson')" ]; then
 
 # in project root
 # fix unknown file extension csljson
 # no idea how to tell vite/rollup to use a "file-loader" for csljson files
 # as suggested for webpack at https://github.com/fiduswriter/citeproc-plus
 
-echo -e '\nmove files: *.csljson -> *.csl.json'
+echo -e '\nmoving files: *.csljson -> *.csl.json'
 mmv \
   'node_modules/citeproc-plus/dist/assets/*.csljson' \
   'node_modules/citeproc-plus/dist/assets/#1.csl.json'
@@ -131,3 +110,5 @@ mmv \
 sed -i.bak -E 's|assets/([^"]+)\.csljson"|assets/\1.csl.json"|g' \
   node_modules/citeproc-plus/dist/*.js \
   node_modules/citeproc-plus/dist/*/*.js
+
+fi
